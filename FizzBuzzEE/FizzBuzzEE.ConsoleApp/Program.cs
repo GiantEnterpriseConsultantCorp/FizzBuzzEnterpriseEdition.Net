7f01b438bfd968e970f4713a6fd3a13b7bd1c689
David Perish <david@stonyhillsoftware.com>
2024-01-17 22:50:27 -0500

    Create totally not enterprise-production-grade POC

---
+++ FizzBuzzEE/FizzBuzzEE.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿Enumerable
+    .Range(1, 100)
+    .ToList()
+    .ForEach(x => 
+        Console.WriteLine(
+            x % 15 == 0 
+                ? "FizzBuzz" 
+                : x % 3 == 0 
+                    ? "Fizz" 
+                    : x % 5 == 0 
+                        ? "Buzz" 
+                        : x));
